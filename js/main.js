fa3b429610f7cfa7bd9d80c051a4a9c92de2d05e
Sebastian <sebatstian@gmail.com>
2019-04-22 15:22:08 -0300

    Slider incluido y posts dinamicos con JSON

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// Configuracion del slider
+$(function () {
+  $('.galeria').bxSlider({
+    mode: 'fade',
+    captions: true,
+    responsive: true,
+    infiniteLoop: true,
+    auto: true,
+    adaptiveHeight: true
+  })
+})
+
+// Configuracion de moment.js en español
+moment.locale('es')
+
+var posts = [
+  {
+    titulo: 'Prueba de titutlo 1',
+    fecha: moment().format('ll'),
+    contenido: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda omnis fugiat suscipit eum hic nemo adipisci veniam! Nihil, laudantium alias deserunt sapiente rem velit inventore, aliquam cum, placeat laborum eos.'
+  },
+  {
+    titulo: 'Prueba de titutlo 2',
+    fecha: moment().format('ll'),
+    contenido: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda omnis fugiat suscipit eum hic nemo adipisci veniam! Nihil, laudantium alias deserunt sapiente rem velit inventore, aliquam cum, placeat laborum eos.'
+  },
+  {
+    titulo: 'Prueba de titutlo 3',
+    fecha: moment().format('ll'),
+    contenido: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda omnis fugiat suscipit eum hic nemo adipisci veniam! Nihil, laudantium alias deserunt sapiente rem velit inventore, aliquam cum, placeat laborum eos.'
+  },
+]
+
+posts.forEach((item) => {
+  var post = `
+    <article class="post">
+      <h2>${item.titulo}</h2>
+      <span class="fecha">${item.fecha}</span>
+      <p>${item.contenido}</p>
+      <a href="" class="btn">Leer más</a>
+    </article>
+  `
+
+  $('#posts').append(post)
+})
+
